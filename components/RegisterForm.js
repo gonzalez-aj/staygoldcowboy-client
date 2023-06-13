77c02fd82fa896bbd9abb00715594ad216d652de
Angie Gonzalez <angiem.go@gmail.com>
2023-06-12 22:22:38 -0500

    user register needs first and last name on form

--- components/RegisterForm.js
+++ components/RegisterForm.js
@@ -6,7 +6,8 @@ import { registerUser } from '../utils/auth'; // Update with path to registerUse
 
 function RegisterForm({ user, updateUser }) {
   const [formData, setFormData] = useState({
-    bio: '',
+    firstName: '',
+    lastName: '',
     uid: user.uid,
   });
 
@@ -15,17 +16,43 @@ function RegisterForm({ user, updateUser }) {
     registerUser(formData).then(() => updateUser(user.uid));
   };
 
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
   return (
-    <Form onSubmit={handleSubmit}>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Gamer Bio</Form.Label>
-        <Form.Control as="textarea" name="bio" required placeholder="Enter your Bio" onChange={({ target }) => setFormData((prev) => ({ ...prev, [target.name]: target.value }))} />
-        <Form.Text className="text-muted">Let other gamers know a little bit about you...</Form.Text>
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
+    <>
+      <Form onSubmit={handleSubmit}>
+        <Form.Text className="text-muted">What&apos;s your name cowboy?</Form.Text>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>First Name</Form.Label>
+          <Form.Control
+            name="firstName"
+            placeholder="Enter your first name"
+            autoComplete="off"
+            onChange={handleChange}
+            required
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Last Name</Form.Label>
+          <Form.Control
+            name="lastName"
+            placeholder="Enter your last name"
+            autoComplete="off"
+            onChange={handleChange}
+            required
+          />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </>
   );
 }
 
