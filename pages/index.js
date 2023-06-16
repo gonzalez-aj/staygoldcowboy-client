e529bf87ff621acd8f22ab2f8879f37c110cd42e
Angie Gonzalez <angiem.go@gmail.com>
2023-06-16 13:41:45 -0500

    styled art cards installed tailwind and daisy failed to import font

--- pages/index.js
+++ pages/index.js
@@ -34,8 +34,8 @@ function Home() {
     <>
       <Head><title>Stay Gold, Cowboy</title></Head>
       <br />
-      <h2> Welcome Partner, {user.first_name} {user.last_name} </h2>
-      <h5>Here&apos;s the art:</h5>
+      <h2 className="text-6xl"> Welcome Partner, {user.first_name} {user.last_name} </h2>
+      <h5 className="text-4xl">Here&apos;s the art:</h5>
       {noArts && <h4>There&apos;s no art here yet</h4>}
       <div className="text-center my-4">
         <div className="d-flex flex-wrap">
