22ed96a0b45473681b99f082eec9e7a128f5a254
Angie Gonzalez <angiem.go@gmail.com>
2023-06-17 23:38:22 -0500

    routed edit button on art-takes to edit page

---
+++ pages/arts/edit/[id].js
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import Head from 'next/head';
+import { getSingleArt } from '../../../utils/data/artData';
+import ArtForm from '../../../components/art/ArtForm';
+
+export default function EditArt() {
+  const [art, setArt] = useState({});
+  const router = useRouter();
+  const { id } = router.query;
+
+  useEffect(() => {
+    getSingleArt(id).then(setArt);
+  }, [id]);
+
+  return (
+    <>
+      <Head>
+        <title>Edit {`${art.title}`}</title>
+      </Head>
+      <ArtForm obj={art} />
+    </>
+  );
+}
