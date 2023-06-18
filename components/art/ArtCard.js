840f76b1586dc3407721ff3125b560589b212d8e
Angie Gonzalez <angiem.go@gmail.com>
2023-06-18 17:28:38 -0500

    delete tags works

--- components/art/ArtCard.js
+++ components/art/ArtCard.js
@@ -2,43 +2,51 @@ import Link from 'next/link';
 import PropTypes from 'prop-types';
 import React from 'react';
 import { Card } from 'react-bootstrap';
+import { deleteArt } from '../../utils/data/artData';
 
-const ArtCard = ({
+export default function ArtCard({
   id,
   title,
   // creationDate,
   imageUrl,
-}) => (
-  <>
-    <div className="card w-96 bg-base-100 shadow-xl">
-      <figure className="px-10 pt-10">
-        <Card.Img src={imageUrl} alt={title} className="xl" />
-      </figure>
-      <div className="card-body items-center text-center">
-        <h2 className="card-title" id="font-white">{title}</h2>
-        {/* <p className="card-text" id="font-white">Creation Date: {creationDate}</p> */}
-        <div className="card-actions">
-          <Link href={`/arts/${id}`} passHref>
-            <Card.Link>Details</Card.Link>
-          </Link>
-          <Link href={`/arts/edit/${id}`} passHref>
-            <Card.Link href="#">Edit</Card.Link>
-          </Link>
-          <Link href={`/arts/edit/${id}`} passHref>
-            <Card.Link href="#">Delete</Card.Link>
-          </Link>
+  onUpdate,
+}) {
+  const deleteThisTag = () => {
+    if (window.confirm(`🛑 Sure you wanna delete ${title}?`)) {
+      deleteArt(id).then(() => onUpdate());
+    }
+  };
+  return (
+    <>
+      <div className="card w-96 bg-base-100 shadow-xl">
+        <figure className="px-10 pt-10">
+          <Card.Img src={imageUrl} alt={title} className="xl" />
+        </figure>
+        <div className="card-body items-center text-center">
+          <h2 className="card-title" id="font-white">{title}</h2>
+          {/* <p className="card-text" id="font-white">Creation Date: {creationDate}</p> */}
+          <div className="card-actions">
+            <Link href={`/arts/${id}`} passHref>
+              <Card.Link>Details</Card.Link>
+            </Link>
+            <Link href={`/arts/edit/${id}`} passHref>
+              <Card.Link href="#">Edit</Card.Link>
+            </Link>
+            <Link href="/" passHref>
+              <Card.Link onClick={deleteThisTag}>Delete</Card.Link>
+            </Link>
+          </div>
         </div>
       </div>
-    </div>
-    <br />
-  </>
-);
+      <br />
+    </>
+  );
+}
 
 ArtCard.propTypes = {
   id: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   // creationDate: PropTypes.string.isRequired,
   imageUrl: PropTypes.string.isRequired,
+  onUpdate: PropTypes.func.isRequired,
 };
-
-export default ArtCard;
